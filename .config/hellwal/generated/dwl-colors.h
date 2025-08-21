3ed9b992c0619091b9157de3a12754a1014774ba
Zoly24 <97642937+Zoly24@users.noreply.github.com>
2025-08-21 14:23:37 -0500

    Delete .config/hellwal directory

--- .config/hellwal/generated/dwl-colors.h
+++
@@ -1,6 +0,0 @@
-#define WALLPAPER "/home/zoly/Pictures/Wallpapers/black-hole-anime.jpg"
-
-static const float rootcolor[]             = COLOR(0x010101ff);
-static const float bordercolor[]           = COLOR(0x2c2a30ff);
-static const float focuscolor[]            = COLOR(0x2c2a30ff);
-static const float urgentcolor[]           = COLOR(0x121235ff);
